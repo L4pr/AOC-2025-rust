5b1798f29791ad2096e53533c4d075e3e18a5031
Renzo <renzo.boenink@gmail.com>
2025-12-05 06:51:05 +0100

    day 5 initial solution

---
+++ src/bin/05.rs
@@ -0,0 +1,127 @@
+#![allow(unused_imports)]
+
+use std::cmp::max;
+use std::collections::HashSet;
+use std::path::Component::ParentDir;
+use advent_of_code::*;
+
+advent_of_code::solution!(5);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut ranges = Vec::new();
+    let mut ids = Vec::new();
+    let mut at_ranges = true;
+    for line in input.lines() {
+        if line.is_empty() {
+            at_ranges = false;
+            continue;
+        }
+        if at_ranges {
+            let bytes = line.as_bytes();
+            let mut split_point_found = false;
+            let mut number1 = 0;
+            let mut number2 = 0;
+            for i in 0..bytes.len() {
+                if bytes[i] == b'-' {
+                    split_point_found = true;
+                    continue;
+                }
+                if !split_point_found {
+                    number1 = number1 * 10 + (bytes[i] - b'0') as u64
+                } else {
+                    number2 = number2 * 10 + (bytes[i] - b'0') as u64
+                }
+            }
+            ranges.push((number1, number2));
+        } else {
+            let bytes = line.as_bytes();
+            let mut n = 0;
+            for &b in bytes {
+                n = n * 10 + (b - b'0') as u64;
+            }
+            ids.push(n);
+        }
+    }
+
+    let mut result = 0;
+    for id in ids {
+        for range in &ranges {
+            if id >= range.0 && id <= range.1 {
+                result += 1;
+                break;
+            }
+        }
+    }
+
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut ranges = Vec::new();
+    for line in input.lines() {
+        if line.is_empty() {
+            break;
+        }
+        let bytes = line.as_bytes();
+        let mut split_point_found = false;
+        let mut number1 = 0;
+        let mut number2 = 0;
+        for i in 0..bytes.len() {
+            if bytes[i] == b'-' {
+                split_point_found = true;
+                continue;
+            }
+            if !split_point_found {
+                number1 = number1 * 10 + (bytes[i] - b'0') as u64
+            } else {
+                number2 = number2 * 10 + (bytes[i] - b'0') as u64
+            }
+        }
+        ranges.push((number1, number2));
+    }
+
+    let mut result = 0;
+    let mut new_ranges: Vec<(u64, u64)> = Vec::new();
+
+    ranges.sort_by_key(|r| r.0);
+
+    for range in &ranges {
+        if new_ranges.is_empty() {
+            new_ranges.push((range.0, range.1));
+            continue;
+        }
+
+        let last_index = new_ranges.len() - 1;
+        let last_range = &mut new_ranges[last_index];
+
+        if range.0 <= last_range.1 + 1 {
+            last_range.1 = max(last_range.1, range.1);
+        } else {
+            new_ranges.push((range.0, range.1));
+        }
+    }
+
+    for range in new_ranges {
+        result += range.1 - range.0 + 1;
+    }
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(14));
+    }
+}
